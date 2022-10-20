b77851c9ae06b54389182c2261642862069b855b
Lio-tm <r0900177@student.thomasmore.be>
2022-10-20 10:20:40 +0200

    create

---
+++ src/routes.js
@@ -0,0 +1,68 @@
+const express = require(`express`);
+const router = express.Router();
+const Campus = require("./models/campus");
+
+router.get("/", (req, res) => {
+  console.log("/ route called");
+  res.send(
+    `<h1>Welcome to my API, these are the available routes:<h1>` +
+      `<h2>/</h2>` +
+      `where you are right now` +
+      `<hr/>` +
+      `<h2>/campus</h2>` +
+      `Returns all campuses in the database using .find()` +
+      `<hr/>`
+  );
+});
+
+router.get("/campus", async (req, res) => {
+  console.log("/campus route called");
+  try {
+    res.json(await Campus.find());
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.get("/campus/:id", async (req, res) => {
+  console.log("/campus/:id route called");
+  try {
+    res.send(await Campus.findById(req.params.id));
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/campus/create", async (req, res) => {
+  console.log("/campus/create route called");
+  try {
+    res.send(await Campus.create(req.body));
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.put("/campus/update/:id", async (req, res) => {
+  console.log("/campus/update/:id route called");
+  try {
+    res.send(await Campus.findByIdAndUpdate(req.params.id, { $set: req.body }));
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.delete("/campus/delete/:id", async (req, res) => {
+  console.log("/campus/delete/:id route called");
+  try {
+    res.send(await Campus.findByIdAndDelete(req.params.id));
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
